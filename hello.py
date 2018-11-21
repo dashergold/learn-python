af3561f7efa06028f60149db10f5bd3cb3a03112
Axel <axel.s.kallen@gmail.com>
2018-11-21 19:50:23 +0100

    the first commit

---
+++ hello.py
@@ -0,0 +1,13 @@
+print ("Hur stor är ena sidan?")
+side1 = int(input ())
+print ("hur stor är andra sidan?")
+side2 = int(input ())
+area = side1*side2
+print ("arean är " + str(area))
+
+print ("Hur stor är basen?")
+base = int(input ())
+print (" Vad är höjden?")
+height = int(input ())
+area = base*height/2
+print ("arean är: " + str(area))
