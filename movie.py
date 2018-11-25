b3d061f8bc7e582aa433c0b81f908dbdf47f9408
Axel <axel.s.kallen@gmail.com>
2018-11-25 18:18:44 +0100

    broke program down into functions

--- movie.py
+++ movie.py
@@ -1,28 +1,19 @@
-#Filmstart
-print("Hej hur gammal är du?")
-sAge = input()
-age = int(sAge)
-if age < 15:
-    print("Du är för ung!")
-else:
-    print("Välkommen in")
-    print("Hur många gram godis skall du köpa?")
-    sCandy = input()
-    candy = int(sCandy)
-    if candy >= 200:
-        print ("diabetiker!")
-    elif candy == 0:
-        print("Bra att du sköter din hälsa")
-    elif candy < 0:
-        print("Snyggt försök!")
-    
-    #Nu börjar filmen!
+#Hämta ett tal från användaren
+def get_int(prompt):
+    print(prompt)
+    sValue = input()
+    return int(sValue)
+
+def print_banner():
+    print("%%%%%%%%%%%%%%%%")
+
+def show_movie(candy):
+     #Nu börjar filmen!
+    print_banner()
     print("tatataatataa...")
     while candy > 0:   
         print("du har "+ str(candy) +" g kvar" )
-        print("Hur många gram godis vill du äta?")
-        sEat = input()
-        eat = int(sEat)
+        eat = get_int("Hur många gram godis vill du äta?")
         if eat < candy:
             print("namnamnam")
             candy = candy - eat
@@ -33,4 +24,28 @@ else:
         else:
             print("så mycket godis har du intel.")
 
+def buy_candy():
+    candy = get_int("Hur många gram godis skall du köpa?")
+    if candy >= 200:
+        print ("diabetiker!")
+    elif candy == 0:
+        print("Bra att du sköter din hälsa")
+    elif candy < 0:
+        print("Snyggt försök!")
+        candy = 0
+    return candy 
+
+#Filmstart
+print_banner()
+age = get_int("Hej hur gammal är du?")
+if age < 15:
+    print("Du är för ung!")
+else:
+    print("Välkommen in")
+
+    candy = buy_candy()
+
+    show_movie(candy) 
+    
+    print_banner()
     print("Tack för besöket!")
