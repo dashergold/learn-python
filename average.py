eeeab33f0ab09a1c176f100ba7c35aa471c48af2
Axel <axel.s.kallen@gmail.com>
2018-11-26 19:52:50 +0100

    new and experienced average

--- average.py
+++ average.py
@@ -1,10 +1,24 @@
-total = 0.0
-n = 0
-while True:
-    sNumber = input()
-    if sNumber == "":
-        break
-    number = int(sNumber)
-    total += number
-    n += 1
-print("The average is "+ str(total/n))
+#total = 0.0
+#n = 0
+#while True:
+#    sNumber = input()
+#    if sNumber == "":
+#        break
+#    number = int(sNumber)
+#    total += number
+#    n += 1
+#print("The average is "+ str(total/n))
+
+def average2(n):
+    avg = (n + 1) / 2
+    print("The average is " +str(avg))
+
+def average(n):
+    total = 0
+    for i in range(n):
+        total += i + 1
+    print("The average is "+ str(total/n))
+
+q = 100000000 
+average2(q)
+average(q)
